0489afa362dd4cdb179755987f78c79b4b273849
mymac <mymac@mymacdeMac.local>
2018-10-07 22:33:16 +0800

    fix install err:Path had bad ownership/permissions

--- ASO/cargo-ios/include/constants.h
+++ ASO/cargo-ios/include/constants.h
@@ -8,7 +8,7 @@
 #define OVERLAY_VIEW_HEIGHT 180
 
 static NSString *COPY_RIGHT = @"ASO菜鸟";
-static NSString *BUILD_VERSION = @"版本: iOS10 zj/180929.000001";
+static NSString *BUILD_VERSION = @"版本: iOS10 cargo/181007.222803";
 
 // task server
 static NSString *TASK_SERVER_HOST = @"127.0.0.1";
